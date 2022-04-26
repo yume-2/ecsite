623218c152392a06e6a92ea590095114b160fc88
yume-2 <181l00198@rissho-univ.jp>
2022-04-26 17:44:22 +0900

    Spring Bootで作成したECサイト

---
+++ src/test/java/com/internous/ecsite/EcsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internous.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
